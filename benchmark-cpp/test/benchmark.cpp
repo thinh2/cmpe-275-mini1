8f92db84c80de11f6c608ce8ea0b77cc9298443b
Thinh Bui <thinh.ducbui@gmail.com>
2024-10-07 16:41:01 -0700

    update testdata folder

--- benchmark-cpp/test/benchmark.cpp
+++ benchmark-cpp/test/benchmark.cpp
@@ -4,13 +4,13 @@
 #include "dataframe.h"
 
 TEST(TestBenchmark, TestAggregationBench) {
-    Data data("/Users/thinhbui/Thinh/Learning/sjsu/fall-2024/cmpe-275/mini-1/benchmark-2/test/testdata/header_no_null.csv", true, false);
+    Data data("testdata/header_no_null.csv", true, false);
     Aggregate<long long> agg("age", AggOp::Sum);
     query_aggregation<long long>(data, agg); 
 }
 
 TEST(TestBenchmark, TestFilterBench) {
-    Data data("/Users/thinhbui/Thinh/Learning/sjsu/fall-2024/cmpe-275/mini-1/benchmark-2/test/testdata/header_no_null.csv", true, false);
+    Data data("testdata/header_no_null.csv", true, false);
     Filter<long long> filter(12, FilterOp::Gt, "age");
     query_filter<long long>(data, filter); 
 }
